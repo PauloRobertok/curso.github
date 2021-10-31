45897b61c48cb3c6830a85225b24081eea467ce2
devPaulo <paulora999@gmail.com>
2021-10-30 22:09:55 -0300

    Nova home finalizada

---
+++ home.js
@@ -0,0 +1,8 @@
+function mostraNome(nome){
+    return nome
+}
+function mostrasSobreNome(sobreNome){
+    return nome
+})
+
+//Finalizado guia da home
--- login.js
+++ login.js
@@ -1,3 +1,10 @@
 // Trabalhando no login
 
 // Finalizando sistema de login
+function login (nome, email) {
+    return 'bem vindo!';
+}
+
+//Seja bem vindo
+
+//Escreva login completa
